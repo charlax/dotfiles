0f3365275a56c031258db50022d691f0377a62d2
Charles-Axel Dein <ca@d3in.org>
2013-11-21 11:06:46 -0800

    Allow passing template as command line arg

--- bin/csv2template.py
+++ bin/csv2template.py
@@ -15,19 +15,32 @@ import jinja2
 def main(args):
 
     csv_file = csv.DictReader(args.csv)
-    template = jinja2.Template(args.template.read())
+
+    template_string = args.template_string
+    if not template_string:
+        template_string = args.template.read()
+    template = jinja2.Template(template_string)
 
     for line in csv_file:
-        args.outfile.write(template.render(**line))
+        args.outfile.write(template.render(**line) + "\n")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Render the template using CSV data")
     parser.add_argument("csv", type=argparse.FileType('rU'))
-    parser.add_argument("template", type=argparse.FileType('r'))
+
+    template_group = parser.add_mutually_exclusive_group()
+    template_group.add_argument(
+        "template", type=argparse.FileType('r'), nargs='?')
+    template_group.add_argument("--template", dest='template_string')
+
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
                         default=sys.stdout)
 
     args = parser.parse_args()
+
+    if not args.template and not args.template_string:
+        parser.error("Either template or --template must be provided")
+
     main(args)
