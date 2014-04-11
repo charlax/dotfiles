0a8d6c9f039aa10c49b3fb62762542e4785ad300
Charles-Axel Dein <ca@d3in.org>
2014-04-10 18:11:46 -0700

    Improve prompt with Mercurial status

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,30 +1,20 @@
-# Thanks to oh-my-zsh
-
 export VIRTUAL_ENV_DISABLE_PROMPT='1'
-
-# Get the name of the branch we are on
-function git_prompt_info() {
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo "(%{$fg[yellow]%}${ref#refs/heads/}%{$reset_color%}$(parse_git_dirty)) "
-}
-
-# Checks if working tree is dirty
-parse_git_dirty() {
-    local SUBMODULE_SYNTAX="--ignore-submodules=dirty"
-
-    if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
-        echo "%{$fg[red]%}*%{$reset_color%}"
-    else
-        echo ""
-    fi
-}
 
 # Get the name of virtual env
 function virtualenv_info {
     [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
 }
 
+# Turn on command substitution in the prompt 
+setopt PROMPT_SUBST
+
+zstyle ':vcs_info:*' enable git svn hg
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' formats "%{$fg[yellow]%}%c%{$reset_color%}(%{$fg[yellow]%}%b%{$reset_color%}%{$fg[red]%}%u%{$reset_color%}) "
+zstyle ':vcs_info:*' unstagedstr '*'
+
 # Prompt
 function precmd() {
-    export PS1="%{$fg[blue]%}%n%{$reset_color%}@%{$fg[blue]%}%m%{$reset_color%} $(git_prompt_info)%{$fg[yellow]%}%. %{$reset_color%}$(virtualenv_info)%% "
+    vcs_info
+    export PROMPT="%{$fg[blue]%}%n%{$reset_color%}@%{$fg[blue]%}%m%{$reset_color%} ${vcs_info_msg_0_}%{$fg[yellow]%}%. %{$reset_color%}$(virtualenv_info)%% "
 }
