1b6956a65003e28c2c599f8b1152fef8fa549f45
Charles-Axel Dein <ca@d3in.org>
2014-06-09 20:55:29 -0700

    Add psaux function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,6 +3,14 @@ psg ()
     ps aux | grep --color=auto $1
 }
 
+function psaux {
+  if [[ -n "$1" ]];then
+    ps aux | head -1 && ps aux | grep "$1" | grep -v grep
+  else
+    echo 'You must supply a grep search expression!'
+  fi
+}
+
 # (f)ind by (n)ame
 # usage: fn foo 
 # to find all files containing 'foo' in the name
