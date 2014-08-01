7ec1614ef51dc6a98d43c3bf316f7c78116667e9
Charles-Axel Dein <ca@d3in.org>
2014-08-01 07:24:08 -0700

    Add cdpath

--- system/path.zsh
+++ system/path.zsh
@@ -10,3 +10,9 @@ export WORKON_HOME="~/.virtualenvs"
 [[ -e /usr/local/share/npm/bin ]] && export PATH="/usr/local/share/npm/bin:$PATH"
 [[ -e /usr/texbin ]] && export PATH="/usr/texbin:$PATH"
 [[ -e /usr/local/bin/virtualenvwrapper.sh ]] && source /usr/local/bin/virtualenvwrapper.sh
+
+
+dropbox_config=$HOME/.dropbox/info.json
+[[ -e $dropbox_config ]] && export DROPBOX_FOLDER=`cat $dropbox_config | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["personal"]["path"]'`
+
+[[ -n ${DROPBOX_FOLDER+x} ]] && export CODE_PATH="$DROPBOX_FOLDER/Code"
