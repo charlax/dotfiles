a4bebbd6cf33a73694b087743d209811cd7ec7a5
Charles-Axel Dein <ca@d3in.org>
2014-08-27 10:04:07 -0700

    Change WORKON_HOME

--- system/path.zsh
+++ system/path.zsh
@@ -2,17 +2,18 @@ export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 export DOTFILES="$HOME/.dotfiles/"
 
-# virtualenvwrapper
-export PROJECT_HOME="$HOME/Documents/Dev"
-export WORKON_HOME="~/.virtualenvs"
-
 [[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc
 [[ -e /usr/local/share/npm/bin ]] && export PATH="/usr/local/share/npm/bin:$PATH"
 [[ -e /usr/texbin ]] && export PATH="/usr/texbin:$PATH"
 [[ -e /usr/local/bin/virtualenvwrapper.sh ]] && source /usr/local/bin/virtualenvwrapper.sh
 
-
 dropbox_config=$HOME/.dropbox/info.json
 [[ -e $dropbox_config ]] && export DROPBOX_FOLDER=`cat $dropbox_config | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["personal"]["path"]'`
 
+export CODE_PATH="$HOME/Documents/Dev"
+# Override if Dropbox is there
 [[ -n ${DROPBOX_FOLDER+x} ]] && export CODE_PATH="$DROPBOX_FOLDER/Code"
+
+# virtualenvwrapper
+export PROJECT_HOME="$CODE_PATH"
+export WORKON_HOME="$HOME/.virtualenvs"
