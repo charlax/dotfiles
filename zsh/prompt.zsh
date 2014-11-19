aa96a4e8302f3ebb2d5974b8bd2de1a61b5c430b
Charles-Axel Dein <ca@d3in.org>
2014-11-19 10:52:15 -0800

    Show marker for untracked files

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,14 +5,29 @@ function virtualenv_info {
     [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
 }
 
-# Turn on command substitution in the prompt 
+# Turn on command substitution in the prompt
 setopt PROMPT_SUBST
 
+zstyle ':vcs_info:*' disable bzr cdv darcs mtn svk tla
 zstyle ':vcs_info:*' enable git svn hg
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' formats "(%{$fg[yellow]%}%b%{$reset_color%}%{$fg[red]%}%u%{$reset_color%}%{$fg[yellow]%}%c%{$reset_color%}) "
 zstyle ':vcs_info:*' unstagedstr '*'
 zstyle ':vcs_info:*' stagedstr '*'
+
+# From vcs_info-examples
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+
++vi-git-untracked(){
+    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+        git status --porcelain | grep '??' &> /dev/null ; then
+        # This will show the marker if there are any untracked files in repo.
+        # If instead you want to show the marker only if there are untracked
+        # files in $PWD, use:
+        #[[ -n $(git ls-files --others --exclude-standard) ]] ; then
+        hook_com[staged]+='*'
+    fi
+}
 
 # Prompt
 function precmd() {
