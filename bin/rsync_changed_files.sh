94dda4181fe593b7aff94c4a6357c2f53be19f64
Charles-Axel Dein <ca@d3in.org>
2014-12-04 11:01:46 -0800

    Improve rsync_changed_files

--- bin/rsync_changed_files.sh
+++ bin/rsync_changed_files.sh
@@ -2,4 +2,16 @@
 
 DIFF_AGAINST='master'
 
+function usage {
+    echo "Usage: $0 rsync_args"
+    exit 0
+}
+
+function check_args {
+    if [[ "$@" = "" ]]; then
+        usage
+    fi
+}
+
+check_args "$@"
 git diff --name-only $DIFF_AGAINST | rsync --files-from=- -avz -e ssh . $@
