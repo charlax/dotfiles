969f829a20821d0395b80f1a51e720a7fce33f4d
Charles-Axel Dein <ca@d3in.org>
2014-12-14 13:45:46 -0800

    Add ssh-reagent

--- system/functions.zsh
+++ system/functions.zsh
@@ -27,3 +27,16 @@ function clip {
 
     perl -ne "\$s=1 if (/$FIRST/); \$s=0 if (/$ENDING/); print \"$PADDING\$_\" if (\$s==1);"
 }
+
+# http://tychoish.com/posts/9-awesome-ssh-tricks/#ssh-reagent
+ssh-reagent () {
+    for agent in $SSH_AUTH_SOCK $STATIC_SSH_AUTH_SOCK /tmp/ssh-*/agent.*; do
+        export SSH_AUTH_SOCK=$agent
+        if ssh-add -l 2>&1 > /dev/null; then
+            echo "Found working SSH Agent: ${SSH_AUTH_SOCK}"
+            ssh-add -L
+            return
+        fi
+    done
+    echo "Cannot find ssh agent - maybe you should reconnect and forward it?"
+}
