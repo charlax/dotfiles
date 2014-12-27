d4aed40ec30f4da3c70da67d6b8932c4aa1cef1d
Charles-Axel Dein <ca@d3in.org>
2014-12-27 15:31:36 -0800

    Do not set custom TZ

--- system/env.zsh
+++ system/env.zsh
@@ -1,9 +1,6 @@
 export EDITOR="vim"
 export EDITOR_GUI="mvim"
 
-# Useful when forwarding TZ env variable via SSH (SendEnv TZ)
-export TZ="America/Los_Angeles"
-
 # Unlimited psql history
 # http://craigkerstiens.com/2013/02/13/How-I-Work-With-Postgres/
 export HISTFILESIZE=
