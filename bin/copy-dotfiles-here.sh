4fe3af695e218db1a07006cdcc11328932e07bdc
Charles-Axel Dein <charles@uber.com>
2015-11-03 21:10:46 -0800

    Change git config setting on copy

--- bin/copy-dotfiles-here.sh
+++ bin/copy-dotfiles-here.sh
@@ -3,3 +3,6 @@ cd .dotfiles
 git archive --format=tar --remote=~/.dotfiles/ HEAD | tar xf -
 cd ..
 python ~/.dotfiles/install.py --symlink-base .
+
+# Some computer have old git version
+sed -i '' -e 's/default = simple/default = current/' .dotfiles/git/gitconfig
