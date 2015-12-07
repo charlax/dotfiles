b543e8c4047cab7cf61b27861a1531bd139afc88
Charles-Axel Dein <charles@uber.com>
2015-12-07 10:50:31 -0800

    Add arp -a to cheatsheet

--- bin/recursive-replace.sh
+++ bin/recursive-replace.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # replace="'s/"$1"/"$2"/g'"
-# find ./ -name "*.py" -print0 | xargs -0 echo "$1 $2" 
+# find ./ -name "*.py" -print0 | xargs -0 echo "$1 $2"
 # DOES NOT WORK
 # find ./ -name "*.py" -print0 | xargs -0 perl -p -i -e $replace
 ack -l $1 | xargs perl -pi -E 's/'$1'/'$2'/g'
