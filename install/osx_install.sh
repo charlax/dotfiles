e47a5dd7f181fe6d419f02189914eab75f29e7e9
Charles-Axel Dein <charles@uber.com>
2016-03-01 14:02:57 +0100

    Add fzf

--- install/osx_install.sh
+++ install/osx_install.sh
@@ -2,4 +2,7 @@ set -x
 set -e
 
 brew update
-brew install -U ack zsh git coreutils zsh-completions rmtrash automake wget mercurial git-flow python python3 autojump hub unrar highlight miller autoenv
+brew install -U ack zsh git coreutils zsh-completions rmtrash automake wget mercurial git-flow python python3 autojump hub unrar highlight miller autoenv fzf
+
+# Install fzf shell bindings
+/usr/local/opt/fzf/install
