d34a87ec235d9e9d9aeb70f800bf2d98d5e5fc47
Charles-Axel Dein <ca@d3in.org>
2017-11-14 13:49:05 +0100

    Add Python local bin path

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,8 @@
 [[ -e /usr/texbin ]] && export PATH="/usr/texbin:$PATH"
-[[ -s `brew --prefix 2> /dev/null`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
+[[ -s `brew --prefix 2> /dev/null`/etc/autojump.sh ]] && . $(brew --prefix)/etc/autojump.sh
 
 dropbox_config=$HOME/.dropbox/info.json
-[[ -e $dropbox_config ]] && export DROPBOX_FOLDER=`cat $dropbox_config | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["personal"]["path"]'`
+[[ -e $dropbox_config ]] && export DROPBOX_FOLDER=$(cat $dropbox_config | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["personal"]["path"]')
 
 export CODE_PATH="$HOME/Documents/Dev"
 # Override if Dropbox is there
@@ -14,7 +14,10 @@ export GOPATH=$HOME/golang
 # Java
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8) >/dev/null 2>&1
 
+# Python
+PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"
+
 # Core paths
-export PATH="/usr/local/opt/gettext/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/bin:$GOPATH/bin"
+export PATH="/usr/local/opt/gettext/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/bin:$GOPATH/bin:$PYTHON_BIN_PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 export DOTFILES="$HOME/.dotfiles/"
