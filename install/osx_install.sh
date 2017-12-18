23d09117db5e9ed377f56814e832242391631038
Charles-Axel Dein <ca@d3in.org>
2017-12-18 16:50:21 +0100

    Add Go

--- install/osx_install.sh
+++ install/osx_install.sh
@@ -3,7 +3,7 @@ set -e
 
 brew update
 brew install macvim --override-system-vim
-brew install -U zsh git coreutils zsh-completions rmtrash automake wget mercurial python python3 autojump hub unrar highlight miller fzf dos2unix the_silver_searcher findutils tldr
+brew install -U zsh git coreutils zsh-completions rmtrash automake wget mercurial python python3 autojump hub unrar highlight miller fzf dos2unix the_silver_searcher findutils tldr go
 
 # Install fzf shell bindings
 /usr/local/opt/fzf/install
