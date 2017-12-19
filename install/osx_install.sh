3e760e4989773a103e4ff25c258b2d2e3e07ef5d
Charles-Axel Dein <ca@d3in.org>
2017-12-19 15:31:50 +0100

    Add a few brew installs

--- install/osx_install.sh
+++ install/osx_install.sh
@@ -5,5 +5,11 @@ brew update
 brew install macvim --override-system-vim
 brew install -U zsh git coreutils zsh-completions rmtrash automake wget mercurial python python3 autojump hub unrar highlight miller fzf dos2unix the_silver_searcher findutils tldr go
 
+echo "Installing fswatch: watch for file changes"
+brew install fswatch
+
+echo "Installing jq: json formatting"
+brew install jq
+
 # Install fzf shell bindings
 /usr/local/opt/fzf/install
