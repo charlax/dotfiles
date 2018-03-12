f16b62c6f102532d3d5394b008412e7dd26b306f
Charles-Axel Dein <ca@d3in.org>
2018-03-12 09:39:08 +0100

    Update npm in update_everything

--- bin/update_everything.sh
+++ bin/update_everything.sh
@@ -18,5 +18,8 @@ pip3 install --user --upgrade pipenv
 echo "Updating Vim dotfiles"
 $VIM_DOTFILES/update-plugions.sh
 
+echo "Updating npm"
+npm i npm
+
 # Rebuild completions
 rm -f ~/.zcompdump; compinit
