52216ead0ace31445e1bede7325b650b0fac0981
Charles-Axel Dein <ca@d3in.org>
2019-10-07 09:03:12 +0200

    Add arp-scan

--- install/install-apps-osx.sh
+++ install/install-apps-osx.sh
@@ -7,8 +7,9 @@ brew install macvim --override-system-vim
 echo "Installing:"
 echo "rg: file searching"
 echo "the_silver_searcher: file searching (ag command)"
+echo "arp-scan: ARP scanner"
 
-brew install -U zsh git coreutils zsh-completions rmtrash automake wget mercurial python python3 autojump hub unrar highlight miller fzf dos2unix the_silver_searcher findutils goa rg
+brew install -U zsh git coreutils zsh-completions rmtrash automake wget mercurial python python3 autojump hub unrar highlight miller fzf dos2unix the_silver_searcher findutils goa rg arp-scan
 
 echo "Installing fswatch: watch for file changes"
 brew install fswatch
