d723e1479ce449c9138c83ff6f764b93ed757ed9
Charles-Axel Dein <ca@d3in.org>
2019-11-25 15:06:22 +0100

    Add a few Debian packages

--- install/install-apps-linux.sh
+++ install/install-apps-linux.sh
@@ -10,7 +10,10 @@ echo "dsniff: includes arpspoof"
 echo "ripgrep: recursively searches directories for a regex pattern"
 echo "gdb: GNU debugger"
 echo "gobuster: busting tool"
-apt-get install -y zsh tldr dsniff ripgrep gdb gobuster
+echo "httpie: http client"
+echo "jq: json query"
+echo "tree: ls in a tree"
+apt-get install -y zsh tldr dsniff ripgrep gdb gobuster httpie jq tree
 
 #
 # SHOULD BE LAST, requires user action
