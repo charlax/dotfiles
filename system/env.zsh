71d29df2cdf3e49a7944be824c4f2b2647fe77f9
Charles-Axel Dein <ca@d3in.org>
2020-01-09 19:38:46 +0100

    Fix EDITOR_GUI

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+
 export EDITOR="vim"
-export EDITOR_GUI="mvim"
-export SHELL=$(which zsh)
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  export EDITOR_GUI="gvim"
+else
+  export EDITOR_GUI="mvim"
+fi
+
+SHELL=$(command -v zsh)
+export SHELL
 export TERM=xterm-256color
 
 [[ -e /usr/local/opt/autoenv/activate.sh ]] && source /usr/local/opt/autoenv/activate.sh
