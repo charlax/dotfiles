01f1384ee00cd154dea8e04f6baed99c1fefb0a5
Charles-Axel Dein <ca@d3in.org>
2020-01-13 21:26:36 +0100

    Add Chrome to Linux

--- install/install-ui-apps-linux.sh
+++ install/install-ui-apps-linux.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
+
+# shellcheck source=../helpers/setup.sh
+. "$(dirname "$0")/../helpers/setup.sh"
+
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+
 sudo apt install slack-desktop vim-gtk3
+
+
+log_info "Installing Google Chrome"
+
+chrome_file="$tmp_dir/google-chrome.deb"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O "$chrome_file"
+sudo dpkg -i "$chrome_file"
