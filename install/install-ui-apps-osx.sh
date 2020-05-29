de05c40f8e7e14d9f477f34d56ffb57fb4e40459
Charles-Axel Dein <ca@d3in.org>
2020-05-29 15:28:50 +0200

    Load iterm configuration

--- install/install-ui-apps-osx.sh
+++ install/install-ui-apps-osx.sh
@@ -32,3 +32,8 @@ apps=(adobe-acrobat-reader
     )
 
 brew cask install "${apps[@]}"
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
