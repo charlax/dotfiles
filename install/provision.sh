38abfbda119de549450ea8a3d32408ff752932ee
Charles-Axel Dein <ca@d3in.org>
2020-08-28 10:41:14 +0200

    Make apt-get really quiet

--- install/provision.sh
+++ install/provision.sh
@@ -5,16 +5,16 @@ APT_GET="apt-get -qq -y"
 
 set -o errexit
 
-sudo $APT_GET update
-sudo DEBIAN_FRONTEND=noninteractive $APT_GET upgrade
-sudo $APT_GET install git curl file build-essential
+sudo $APT_GET update > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive $APT_GET upgrade > /dev/null
+sudo $APT_GET install git curl file build-essential > /dev/null
 
 # Install homebrew
 CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 DOTFILES="$HOME/.dotfiles"
-git clone -q https://github.com/charlax/dotfiles.git $DOTFILES
+git clone -q https://github.com/charlax/dotfiles.git "$DOTFILES"
 
-python3 $DOTFILES/install.py --with-all --symlink-force
+python3 "$DOTFILES/install.py" --with-all --symlink-force
 
 ~/.dotfiles/install/install-apps-all.sh
