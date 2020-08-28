93f8fffd3600713d5389e6992fb388bca2588274
Charles-Axel Dein <ca@d3in.org>
2020-08-28 10:57:59 +0200

    Add echos in provision

--- install/provision.sh
+++ install/provision.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-# Provision a new machine
+
+echo "Provisioning a new machine..."
 
 APT_GET="apt-get -qq -y"
 
 set -o errexit
 
+echo "Updating packages"
 sudo $APT_GET update > /dev/null
 sudo DEBIAN_FRONTEND=noninteractive $APT_GET upgrade > /dev/null
+
+echo "Installing base packages"
 sudo $APT_GET install git curl file build-essential > /dev/null
 
 # Install homebrew
+echo "Installing homebrew"
 CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo "Installing dotfiles"
 DOTFILES="$HOME/.dotfiles"
 git clone -q https://github.com/charlax/dotfiles.git "$DOTFILES"
 
