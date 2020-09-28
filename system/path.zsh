b862a4b86ac5261949c43feca2dacb4f431503b8
Charles-Axel Dein <ca@d3in.org>
2020-09-28 10:18:07 +0200

    Add check for unpushed code in clean up

--- system/path.zsh
+++ system/path.zsh
@@ -9,17 +9,8 @@ fi
 [[ -s $(brew --prefix 2> /dev/null)/etc/autojump.sh ]] && . "$(brew --prefix)"/etc/autojump.sh
 [[ -s /usr/share/autojump/autojump.sh ]] && . /usr/share/autojump/autojump.sh
 
-dropbox_config=$HOME/.dropbox/info.json
-if [[ -e $dropbox_config ]]; then
-    DROPBOX_FOLDER=$(python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["personal"]["path"])' < "$dropbox_config")
-    export DROPBOX_FOLDER
-fi
-
-export CODE_PATH="$HOME/Documents/Code"
-export FORK_PATH="$HOME/Documents/code_forks"
-# Override if Dropbox is there
-[[ -n ${DROPBOX_FOLDER+x} ]] && export CODE_PATH="$DROPBOX_FOLDER/Code"
-[[ -n ${DROPBOX_FOLDER+x} ]] && export FORK_PATH="$DROPBOX_FOLDER/code_forks"
+export CODE_PATH="$HOME/CodePerso"
+export FORK_PATH="$HOME/Forks"
 
 # Go
 export GOPATH=$CODE_PATH/golang
