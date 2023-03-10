aa7fb9ce56bf215e470d77b0ba394c194bfb6f78
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2023-03-09 20:18:14 -0500

    Add lots of cheats

--- cheatsheets/argparse.py
+++ cheatsheets/argparse.py
@@ -1,6 +1,13 @@
 # Setup argprase
 import argparse
 import sys
+from typing import TextIO
+
+
+def main(infile: TextIO) -> int:
+    infile.close()
+    return 0
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="description")
@@ -12,3 +19,4 @@ if __name__ == "__main__":
     )
     parser.add_argument("--release", action="store_true", help="store true")
     args = parser.parse_args()
+    sys.exit(main(infile=args.infile))
--- cheatsheets/pandas.py
+++ cheatsheets/pandas.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-# Change number of displayed columns
+# Change number of displayed columns or rows
 pd.options.display.max_columns = 6
+pd.options.display.max_rows = 1000
+
+# Remove empty strings (does not work with NaN)
+df = df[df["latitude"].astype(bool)]
+
+# Drop NaN
+df.dropna(subset=["latitude"], inplace=True)
+
+# Filter rows (multiple conditions)
+df.loc[(df["created_at"] >= "2023-02-22") & (df["created_at"] <= "2023-02-22")]
