ab42f65576c1548858c33023212fc7dd70b5d343
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2026-02-01 13:47:23 -0500

    Remove unused files

--- bin/highlight_for_keynote.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Quick shortcut to highlight some code for copy pasting into Keynote
-# Took inspiration from https://gist.github.com/jimbojsb/1630790
-
-highlight -k Consolas  --font-size=34 -O rtf "$1" | pbcopy
