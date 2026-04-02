ae32af3600dc0716a94661337973524a7eae06d0
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2026-04-01 23:05:14 -0400

    Update font for neovide

--- vim/init.lua
+++ vim/init.lua
@@ -1,10 +1,14 @@
 if vim.g.neovide then
+  vim.o.guifont = "RobotoMono Nerd Font:h14"
   vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
   vim.keymap.set('v', '<D-c>', '"+y') -- Copy
   vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
   vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
   vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
   vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  -- Disable cursor animation
+  vim.g.neovide_cursor_animation_length = 0
 end
 
 -- Allow clipboard copy paste in neovim
